a2904ee8110f34f31a9278478783e0c8a538b4c3
Nicholas Koh <nicholaskhs@hotmail.com>
2020-10-18 18:04:34 +0800

    Update modifiers for phase checks

--- contracts/Registrar.sol
+++ contracts/Registrar.sol
@@ -26,7 +26,7 @@ contract Registrar {
     // ******** Bidding functionality ********
 
     // Bid and reveal phase lengths, counted in block numbers
-    uint constant bidLength = 3;
+    uint constant commitLength = 3;
     uint constant revealLength = 3;
     
     // New Bidding info struct will be created for each new domain name bid initiated
@@ -36,7 +36,7 @@ contract Registrar {
         mapping (address => bytes32) commits;
         
         // Expiry blocktime for bids to be added
-        uint bidExpiry;
+        uint commitExpiry;
         
         // Expiry blocktime for bids to be revealed
         uint revealExpiry;
@@ -49,19 +49,38 @@ contract Registrar {
 
     // ******** Commit phase ********
 
-    // Check for existing bid
+    // Check for inactive bid
+    modifier biddingInactive(string memory _name) {
+        require(!bids[_name].active);
+        _;
+    }
+    
+    // Check for active bid
     modifier biddingActive(string memory _name) {
-        require (bids[_name].active);
+        require(bids[_name].active);
+        _;
+    }
+    
+    // Check for block height < commit expiry
+    modifier commitPhase(string memory _name) {
+        require(block.number <= bids[_name].commitExpiry);
+        _;
+    }
+    
+    // Check for commit expiry < block height < reveal expiry
+    modifier revealPhase(string memory _name) {
+        require(block.number > bids[_name].commitExpiry);
+        require(block.number <= bids[_name].revealExpiry);
         _;
     }
     
     // Start a new domain name bid
-    function startBid(string memory _name, bytes32 _commit) public {
+    function startBid(string memory _name, bytes32 _commit) public biddingInactive(_name) {
         Bidding storage b = bids[_name];
         
         // Set bid and reveal expiry times
-        b.bidExpiry = block.number + bidLength;
-        b.revealExpiry = block.number + bidLength + revealLength;
+        b.commitExpiry = block.number + commitLength;
+        b.revealExpiry = block.number + commitLength + revealLength;
         
         // Assign hashed commit to address in bid info mapping
         b.commits[msg.sender] = _commit;
@@ -70,7 +89,8 @@ contract Registrar {
         b.active = true;
     }
     
-    function addBid(string memory _name, bytes32 _commit) public biddingActive(_name) {
+    // Add to an active bid
+    function addBid(string memory _name, bytes32 _commit) public biddingActive(_name) commitPhase(_name) {
         Bidding storage b = bids[_name];
         
         // Assign hashed commit to address in bid info mapping
