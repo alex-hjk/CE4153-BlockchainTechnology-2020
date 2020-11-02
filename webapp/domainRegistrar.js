e5234c66c1e8c02fb7076f6aba5cc326c9a923fc
Nicholas Koh <nicholaskhs@hotmail.com>
2020-11-02 17:23:17 +0800

    Contract config setup

--- webapp/domainRegistrar.js
+++ webapp/domainRegistrar.js
@@ -1,16 +1,20 @@
 import { useState, useRef, useEffect } from "react";
-import detectEthereumProvider from "@metamask/detect-provider";
+
 // NOTE: be aware of this: https://flaviocopes.com/parcel-regeneratorruntime-not-defined/
 import Web3 from "web3";
 
 // importing a compiled contract artifact which contains function signature etc. to interact
-import artifact from "../build/contracts/Registrar.json";
+import registrarArtifact from "../build/contracts/Registrar.json";
+import bidderArtifact from"../build/contracts/Bidder.json"
 
-const myAddress = "0xcc6b9a2Ef844002c413d992B980EeB7b08899A10"; // PLEASE CHANGE IT TO YOURS
+// Contract setup - to update after deployment
+export const registrarAddress = "0xB280Db02eFdb0c940926d7B92F9Fc24aBffaa9C2";
+export const bidderAddress = "";
+
+// Web3 provider endpoints
 const infuraWSS = `wss://ropsten.infura.io/ws/v3/dfe7b73d377740b69fefd0ed7a8b104d`; // PLEASE CHANGE IT TO YOURS
 const localGanache = "ws://127.0.0.1:7545" // Local Ganache RPC address
 
-export const DomainRegistrarContractAddress = "0xB280Db02eFdb0c940926d7B92F9Fc24aBffaa9C2"; // PLEASE CHANGE IT TO YOURS
 export const Testnet = "ropsten"; // PLEASE CHANGE IT TO YOURS
 
 // Set up web3 provider
@@ -24,8 +28,9 @@ if (window.ethereum) {
   web3 = new Web3(new Web3.providers.WebsocketProvider(localGanache));
 }
 
-// doc here: https://web3js.readthedocs.io/en/v1.2.11/web3.html#providers
-const contract = new web3.eth.Contract(artifact.abi, DomainRegistrarContractAddress);
+// Set up contract objects
+const contract = new web3.eth.Contract(registrarArtifact.abi, registrarAddress);
+const contract = new web3.eth.Contract(bidderArtifact.abi, bidderAddress);
 
 export const querySpecificDomain = async (domainName) => {
   // doc here: https://web3js.readthedocs.io/en/v1.2.11/web3-eth-contract.html#methods-mymethod-call
