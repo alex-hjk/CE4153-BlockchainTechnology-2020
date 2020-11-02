a1722661c9a37219380caa2f6491004e409d3e3d
Nicholas Koh <nicholaskhs@hotmail.com>
2020-11-02 17:48:51 +0800

    Brief config cleanup

--- webapp/domainRegistrar.js
+++ webapp/domainRegistrar.js
@@ -1,9 +1,10 @@
 import { useState, useRef, useEffect } from "react";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 // NOTE: be aware of this: https://flaviocopes.com/parcel-regeneratorruntime-not-defined/
 import Web3 from "web3";
 
-// importing a compiled contract artifact which contains function signature etc. to interact
+// Import compiled contract artifacts for function interaction
 import RegistrarArtifact from "../build/contracts/Registrar.json";
 import BidderArtifact from"../build/contracts/Bidder.json"
 
@@ -12,10 +13,10 @@ export const RegistrarAddress = "0xB280Db02eFdb0c940926d7B92F9Fc24aBffaa9C2";
 export const BidderAddress = "";
 
 // Web3 provider endpoints
-const infuraWSS = `wss://ropsten.infura.io/ws/v3/dfe7b73d377740b69fefd0ed7a8b104d`; // PLEASE CHANGE IT TO YOURS
+const infuraWSS = `wss://ropsten.infura.io/ws/v3/dfe7b73d377740b69fefd0ed7a8b104d`;
 const localGanache = "ws://127.0.0.1:7545" // Local Ganache RPC address
 
-export const Testnet = "ropsten"; // PLEASE CHANGE IT TO YOURS
+export const Testnet = "localhost";
 
 // Set up web3 provider
 let web3;
@@ -33,7 +34,6 @@ const contract = new web3.eth.Contract(RegistrarArtifact.abi, RegistrarAddress);
 const contract = new web3.eth.Contract(BidderArtifact.abi, BidderAddress);
 
 export const querySpecificDomain = async (domainName) => {
-  // doc here: https://web3js.readthedocs.io/en/v1.2.11/web3-eth-contract.html#methods-mymethod-call
   const {domainOwner, domainExpiry} = await contract.methods.getSpecificDomainDetails(domainName).call({ from: addr });
   return { owner: domainOwner, expiry: domainExpiry };
 };
