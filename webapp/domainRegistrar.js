c5b733f312ea15c4405fb48ad1ae379a1d443633
Nicholas Koh <nicholaskhs@hotmail.com>
2020-11-02 14:32:51 +0800

    Fix web3.js local link for react

--- webapp/domainRegistrar.js
+++ webapp/domainRegistrar.js
@@ -8,13 +8,22 @@ import artifact from "../build/contracts/Registrar.json";
 
 const myAddress = "0xcc6b9a2Ef844002c413d992B980EeB7b08899A10"; // PLEASE CHANGE IT TO YOURS
 const infuraWSS = `wss://ropsten.infura.io/ws/v3/dfe7b73d377740b69fefd0ed7a8b104d`; // PLEASE CHANGE IT TO YOURS
+const localGanache = "http://127.0.0.1:7545" // Local Ganache RPC address
 
 export const DomainRegistrarContractAddress = "0xB280Db02eFdb0c940926d7B92F9Fc24aBffaa9C2"; // PLEASE CHANGE IT TO YOURS
 export const Testnet = "ropsten"; // PLEASE CHANGE IT TO YOURS
 
-const web3 = new Web3(
-  Web3.currentProvider || new Web3.providers.WebsocketProvider(infuraWSS)
-);
+// Set up web3 provider
+let web3;
+
+if (window.ethereum) {
+  web3 = new Web3(window.ethereum);
+  window.ethereum.enable();
+} else {
+  console.warn(`No web3 detected, falling back on external provider.`);
+  web3 = new Web3(new Web3.providers.HttpProvider(localGanache));
+}
+
 // doc here: https://web3js.readthedocs.io/en/v1.2.11/web3.html#providers
 const contract = new web3.eth.Contract(artifact.abi, DomainRegistrarContractAddress);
 
